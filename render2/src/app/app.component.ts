891e72b8777ed332c2bb855a3beaa81348ad1a34
mantuonweb <mantu.nigam@globallogic.com>
2018-02-26 15:57:11 +0530

    initial comit

--- render2/src/app/app.component.ts
+++ render2/src/app/app.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'app works!';
-	arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+	arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+	loadMore() {
+		this.arr.push.apply(this.arr, this.arr);
+	}
 }
--- render2/src/app/loadmore.directive.ts
+++ render2/src/app/loadmore.directive.ts
@@ -1,17 +1,31 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2, Output, EventEmitter } from '@angular/core';
 
 @Directive({
-	selector: '[appLoadmore]'
+	selector: '[scroller]'
 })
 export class LoadmoreDirective {
-
-	constructor(private renderer: Renderer2, private el: ElementRef) { }
+	@Output() onReachedBottom = new EventEmitter();
+	constructor(private renderer: Renderer2, private el: ElementRef) {
+		console.log('e');
+	}
 
 	ngOnInit() {
 		this.renderer.setProperty(this.el.nativeElement, 'alt', 'Cute alligator');
 	}
-	@HostListener('scroll', ['$event']) private onScroll($event: Event): void {
-		console.log($event.srcElement.scrollLeft, $event.srcElement.scrollTop);
-	};
+	@HostListener('scroll', ['$event']) scrolling(event) {
+		let tracker = event.target;
+
+		let limit = tracker.scrollHeight - tracker.clientHeight;
+		console.log(event.target.scrollTop, limit);
+		if (event.target.scrollTop === limit) {
+			console.log('end reached');
+			this.onReachedBottom.emit();
+		}
+		console.log('scrolling');
+	}
+
+	@HostListener('click') clicking() {
+		console.log('clicking...');
+	}
 
 }
